5565e18de260af44d91a224617c47bc1cda7e3d8
zhxxstar <zhxxstar@163.com>
2026-03-06 14:45:21 +0800

    Add luci-app-lucky to package list

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -60,7 +60,7 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-app-store"
-PACKAGES="$PACKAGES openssh-sftp-server"
+PACKAGES="$PACKAGES luci-app-lucky"
 
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
